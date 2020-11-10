1d468a2618c68ca64d5c464be585660f5a1e8cc2
Yuriy Protsyk <Yuriy_Protsyk@epam.com>
2020-11-10 23:22:15 +0200

    tests related to grid fail

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,12 +2,15 @@ import { TestBed, inject, ComponentFixture } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { GetReposService, Repository } from './services/get-repos.service';
 import { of, throwError } from 'rxjs';
+import { AgGridModule } from 'ag-grid-angular';
+import { GridOptions } from 'ag-grid-community';
 
 describe('AppComponent', () => {
   let component: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
   let getReposSpy: jasmine.Spy;
   let testReposForTable: Repository[];
+  let gridOptions: GridOptions = <GridOptions>{};
 
   beforeEach(async () => {
     testReposForTable = [
@@ -33,10 +36,8 @@ describe('AppComponent', () => {
 
     await TestBed.configureTestingModule({
       declarations: [AppComponent],
-      providers: [
-        AppComponent,
-        { provide: GetReposService, useValue: getReposService },
-      ],
+      providers: [{ provide: GetReposService, useValue: getReposService }],
+      imports: [AgGridModule.withComponents([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AppComponent);
@@ -49,9 +50,8 @@ describe('AppComponent', () => {
 
   it('should contain interpolated element', () => {
     fixture.detectChanges();
-    const app = fixture.componentInstance;
     const h1 = fixture.nativeElement.querySelector('h1');
-    expect(h1.textContent).toContain(app.title);
+    expect(h1.textContent).toContain(component.title);
   });
 
   it('should render title', () => {
@@ -63,5 +63,23 @@ describe('AppComponent', () => {
   it('should call getReposSpy', () => {
     fixture.detectChanges();
     expect(getReposSpy.calls.any()).toBe(true, 'getRepos called');
+  });
+
+  it('grid API is not available until  `detectChanges`', () => {
+    expect(component.gridOptions.api).not.toBeTruthy();
+  });
+
+  it('grid API is available after `detectChanges`', () => {
+    fixture.detectChanges();
+    expect(component.gridOptions.api).toBeTruthy();
+  });
+  it('the grid cells should be as expected', () => {
+    const appElement = fixture.nativeElement;
+    const grid = appElement.querySelector('ag-grid-angular');
+    const headerElements = grid.querySelectorAll('.ag-header-cell-text');
+    expect(headerElements.length).toBe(3);
+    // expect(cellElements[0].textContent).toEqual("Test Name");
+    // expect(cellElements[1].textContent).toEqual("42");
+    // expect(cellElements[2].textContent).toEqual("84");
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,32 @@
 import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 import { GetReposService, Repository } from './services/get-repos.service';
-
+import { GridApi, GridOptions } from 'ag-grid-community';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   public title = 'My Git Repos';
+  public gridOptions: GridOptions;
   public columnDefs = [
     { field: 'name', sortable: true, filter: true },
     { field: 'language', sortable: true, filter: true },
     { field: 'size', sortable: true, filter: true },
-    { field: 'star', sortable: true, filter: true }
+    { field: 'star', sortable: true, filter: true },
   ];
   public rowRepositoryData$: Observable<Repository[]>;
 
-  constructor(public getReposService: GetReposService) {
+  constructor(public getReposService: GetReposService) {}
 
-  }
   ngOnInit(): void {
     this.rowRepositoryData$ = this.getReposService.getRepos();
+  }
+
+  onGridReady(gridOptionsIncome) {
+    console.log(gridOptionsIncome);
+    this.gridOptions = gridOptionsIncome;
   }
 }
